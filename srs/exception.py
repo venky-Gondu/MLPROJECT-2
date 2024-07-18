486f16cdeac2b5bcba3341a132806fc54b00d81d
venky <venkatesh.21mis7170@vitapstudent.ac.in>
2024-07-18 18:36:11 +0530

    new files

--- srs/exception.py
+++ srs/exception.py
@@ -1,14 +1,19 @@
-import sys 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-        file_name,exc_tb.tb_lineno,str(str))
-        
+import sys
+from srs.logger import logging
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message[{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
     return error_message
+
 class CustomException(Exception):
-    def __init__(self,erro_message,error_detail:sys):
-        super.__init__(erro_message)
-        self.error_message=error_message_detail(erro_message,error_detail=error_detail)
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)  # Correct way to call the super class __init__
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+
     def __str__(self):
         return self.error_message
+
--- srs/logger.py
+++ srs/logger.py
@@ -1,18 +1,24 @@
 import logging
 import os
 from datetime import datetime
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# Generate a log file name with the current datetime
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
+# Define the directory for log files
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+# Full path to the log file
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+# Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s]%(linemo)d %(name)s -%(levelname)s - %(message)s",
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-
 )
 
-if __name__=="__main__":
-    logging.info("Logging has stored")
+# Test logging
+if __name__ == "__main__":
+    logging.info("Logging has started")
